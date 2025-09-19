8f5f0ba3b51dd3d27b3a401c6e640ed518dbe691
sifanfiseha <sifen.fiseha@astu.edu.et>
2025-09-19 18:32:35 +0300

    feat: add custom Tailwind CSS variables and dark mode variant

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,39 +1,20 @@
 import React from "react";
-import { useAppDispatch, useAppSelector } from "../app/hooks";
-import { Toaster } from "react-hot-toast";
-import {
-  notifyError,
-  notifySuccess,
-  toggleTheme,
-} from "../features/ui/uiSlice";
+import { useAppDispatch } from "../app/hooks";
+
+import { toggleTheme } from "../features/ui/uiSlice";
 
 const Home: React.FC = () => {
-  const { theme } = useAppSelector((s) => s.ui);
   const dispatch = useAppDispatch();
   return (
     <>
-      <Toaster
-        position="bottom-right"
-        toastOptions={{
-          style: {
-            background: theme === "dark" ? "#000" : "#fff",
-            color: theme === "dark" ? "#fff" : "#000",
-            border: `1px solid ${theme === "dark" ? "#333" : "#ddd"}`,
-          },
-          success: {
-            iconTheme: {
-              primary: theme === "dark" ? "#fff" : "#000",
-              secondary: theme === "dark" ? "#000" : "#fff",
-            },
-          },
-          error: {
-            iconTheme: {
-              primary: theme === "dark" ? "#fff" : "#000",
-              secondary: theme === "dark" ? "#000" : "#fff",
-            },
-          },
-        }}
-      />
+      <div className="min-h-screen w-full dark:bg-black">
+        <button
+          className="dark:text-white "
+          onClick={() => dispatch(toggleTheme())}
+        >
+          toogle
+        </button>
+      </div>
     </>
   );
 };
