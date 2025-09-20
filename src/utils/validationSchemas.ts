1d2b284ad2ce5b8ad2ef21fc781d76f106fd0a82
sifanfiseha <sifen.fiseha@astu.edu.et>
2025-09-20 19:05:23 +0300

    feat(profile): add Zod validation for profile update form inputs

--- src/utils/validationSchemas.ts
+++ src/utils/validationSchemas.ts
@@ -2,6 +2,7 @@ import z from "zod";
 
 export type LoginInput = z.infer<typeof loginSchema>;
 export type SignupInput = z.infer<typeof signupSchema>;
+export type ProfileInput = z.infer<typeof profileSchem>;
 
 export const loginSchema = z.object({
   email: z.string().email("Invalid email address"),
@@ -13,4 +14,10 @@ export const signupSchema = z.object({
   last_name: z.string().min(2, "Last name is too short"),
   email: z.string().email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+export const profileSchem = z.object({
+  first_name: z.string().min(2, "First name is too short"),
+  last_name: z.string().min(2, "Last name is too short"),
+  email: z.string().email("Invalid email address"),
 });
