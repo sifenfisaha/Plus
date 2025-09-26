53c186014829080a6c26ec795abf4c86cf21ee93
sifen fisaha <sifenfisaha@gmail.com>
2025-09-26 21:49:36 +0300

    fix(footer): fix button navigation

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
+import blogReducer from '../features/blogs/blogSlice'
 import uiReducer from "../features/ui/uiSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     ui: uiReducer,
+    blog: blogReducer
   },
 });
 
