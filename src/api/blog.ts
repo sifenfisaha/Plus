473f5bc877029b044ff5a0b4228d78192b6c8a6c
sifen fisaha <sifenfisaha@gmail.com>
2025-09-28 17:11:20 +0300

    feat(blog): add fetchBlog by ID, bookmarkBlog,likeBlog and addComment API calls

--- src/api/blog.ts
+++ src/api/blog.ts
@@ -1,10 +1,11 @@
 import api from "../utils/axios";
+import type { commentInput } from "../utils/validationSchemas";
 
 interface Para {
   pageParam?: number;
   filter: string;
   search?: string;
-  tags?: string
+  tags?: string;
 }
 
 export const fetchPublishedBlogs = async (data: Para) => {
@@ -15,9 +16,35 @@ export const fetchPublishedBlogs = async (data: Para) => {
       sortBy: data.filter,
       search: data.search,
       order: "desc",
-      tags: data.tags
+      tags: data.tags,
     },
   });
+  return res.data;
+};
+
+export const fetchBlog = async (id: string) => {
+  const res = await api.get(`/blog/${id}`);
+  return res.data;
+};
+
+export const likeBlog = async (id: string) => {
+  const res = await api.post(`/like/${id}/toggle`);
+  return res.data;
+};
+
+export const bookmarkBlog = async (id: string) => {
+  const res = await api.post(`/bookmarks/${id}/toggle`);
+  return res.data;
+};
+
+export const addComment = async ({
+  comment,
+  id,
+}: {
+  id: string;
+  comment: commentInput;
+}) => {
+  const res = await api.post(`/comment/${id}`, comment);
   return res.data;
 };
 
