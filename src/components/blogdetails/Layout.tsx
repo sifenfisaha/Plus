38bb861e0ba261dd45232234b84718961ae813fc
sifen fisaha <sifenfisaha@gmail.com>
2025-10-04 10:16:33 +0300

    feat(blog): add owner-only edit button and loading skeleton for blog detail
    - added edit button on blog detail page, visible only to blog owner
    - implemented loading skeleton UI for blog detail while fetching data

--- src/components/blogdetails/Layout.tsx
+++ src/components/blogdetails/Layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useBlog } from "../../features/blogs/hooks";
 import { useParams } from "react-router-dom";
 import Blog from "./component/Blog";
+import BlogSkeleton from "./component/BlogLoading";
 
 const Layout: React.FC = () => {
   const { id } = useParams();
@@ -10,7 +11,8 @@ const Layout: React.FC = () => {
   return (
     <>
       {isPending ? (
-        <p className="dark:text-white">loading...</p>
+        // <p className="dark:text-white">loading...</p>
+        <BlogSkeleton />
       ) : (
         <Blog blog={data.blog} />
       )}
